378718047347dd8f9ff469161bc4d90f3b48ccd0
ab siddik <absiddik96@gmail.com>
2018-10-31 13:01:44 +0600

    footer design update complete

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -107,9 +107,137 @@
 				@yield('content')
 
 				<footer>
+					<div style="padding:0 0 40px 0; " id="footer">
+						<div style="background: #525457; padding-top: 60px">
+							<div class="container">
+								<div class="row row-bottom-padded-md">
+									<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+										<h3>About Travel</h3>
+										<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+									</div>
+									<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+										<h3>Top Hotels</h3>
+										<ul>
+											<li><a href="#">Boracay Hotel</a></li>
+											<li><a href="#">Dubai Hotel</a></li>
+											<li><a href="#">Singapore Hotel</a></li>
+											<li><a href="#">Manila Hotel</a></li>
+										</ul>
+									</div>
+									<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+										<h3>Best Places</h3>
+										<ul>
+											<li><a href="#">Boracay Beach</a></li>
+											<li><a href="#">Dubai</a></li>
+											<li><a href="#">Singapore</a></li>
+											<li><a href="#">Hongkong</a></li>
+										</ul>
+									</div>
+									<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+										<h3>Social Media</h3>
+										<ul>
+											<li><a href="#">Facebook</a></li>
+											<li><a href="#">Twitter</a></li>
+											<li><a href="#">YouTube</a></li>
+										</ul>
+									</div>
+									<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+										<h3>User Account</h3>
+										<ul>
+											<li><a href="#">Sign in</a></li>
+											<li><a href="#">Sign up</a></li>
+											<li><a href="#">Account settings</a></li>
+										</ul>
+									</div>
+									<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+										<h3>Company</h3>
+										<ul>
+											<li><a href="#">About Us</a></li>
+											<li><a href="#">Terms & Conditions</a></li>
+											<li><a href="#">Privacy</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div style="background: #393e46; padding-top: 25px" class="">
+							<div class="container">
+								<div class="row">
+									<div class="col-md-6 text-left">
+										<p>© {{ date('Y') }} {{ config('app.name')}}, All right reserved.</p>
+									</div>
+									<div class="col-md-6 text-right">
+										<div style="line-height: 8px">
+											<p><b>Corporate office</b></p>
+											<p>House: 45, Road: 13/C, Block: E, Banani, Dhaka, Bangladesh</p>
+										</div>
+										<div style="line-height: 8px">
+											<p><b>Contact us</b></p>
+											<p>Email: ask@tb-bd.com </p>
+											<p>Phone: +8809617617617</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</footer>
+
+
+				{{-- <footer>
 					<div id="footer">
 						<div class="container">
-
+							<div class="row row-bottom-padded-md">
+								<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+									<h3>About Travel</h3>
+									<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+								</div>
+								<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+									<h3>Top Flights Routes</h3>
+									<ul>
+										<li><a href="#">Manila flights</a></li>
+										<li><a href="#">Dubai flights</a></li>
+										<li><a href="#">Bangkok flights</a></li>
+										<li><a href="#">Tokyo Flight</a></li>
+										<li><a href="#">New York Flights</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+									<h3>Top Hotels</h3>
+									<ul>
+										<li><a href="#">Boracay Hotel</a></li>
+										<li><a href="#">Dubai Hotel</a></li>
+										<li><a href="#">Singapore Hotel</a></li>
+										<li><a href="#">Manila Hotel</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+									<h3>Interest</h3>
+									<ul>
+										<li><a href="#">Beaches</a></li>
+										<li><a href="#">Family Travel</a></li>
+										<li><a href="#">Budget Travel</a></li>
+										<li><a href="#">Food &amp; Drink</a></li>
+										<li><a href="#">Honeymoon and Romance</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+									<h3>Best Places</h3>
+									<ul>
+										<li><a href="#">Boracay Beach</a></li>
+										<li><a href="#">Dubai</a></li>
+										<li><a href="#">Singapore</a></li>
+										<li><a href="#">Hongkong</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2 col-sm-2 col-xs-12 fh5co-footer-link">
+									<h3>Affordable</h3>
+									<ul>
+										<li><a href="#">Food &amp; Drink</a></li>
+										<li><a href="#">Fare Flights</a></li>
+									</ul>
+								</div>
+							</div>
 							<div class="row">
 								<div class="col-md-6 col-md-offset-3 text-center">
 									<p class="fh5co-social-icons">
@@ -119,12 +247,14 @@
 										<a href="#"><i class="icon-dribbble2"></i></a>
 										<a href="#"><i class="icon-youtube"></i></a>
 									</p>
-									<p>Copyright 2016 Free Html5 <a href="#">Module</a>. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a href="" target="_blank">Freehtml5.co</a> / Demo Images: <a href="" target="_blank">Unsplash</a></p>
+									<p>Copyright 2016 Free Html5 <a href="#">Module</a>. All Rights Reserved. <br>Made with <i class="icon-heart3"></i> by <a href="http://freehtml5.co/" target="_blank">Freehtml5.co</a> / Demo Images: <a href="https://unsplash.com/" target="_blank">Unsplash</a></p>
 								</div>
 							</div>
 						</div>
 					</div>
-				</footer>
+				</footer> --}}
+
+
 
 
 
