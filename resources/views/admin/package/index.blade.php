2b90968e6222ddefdd156d816988abf1418ba5ed
ab siddik <absiddik96@gmail.com>
2018-11-15 16:52:25 +0600

    total seat add in update & some problem fixed

--- resources/views/admin/package/index.blade.php
+++ resources/views/admin/package/index.blade.php
@@ -2,78 +2,82 @@
 
 @section('content')
     <div class="row clearfix">
-        <div class="card">
-            <div class="header">
-                <h2>Package</h2>
-                <ul class="header-dropdown m-r--5">
-                    <li class="dropdown">
-                        <a class="btn btn-primary" href="{!! route('packages.create') !!}">+ Add New</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="body">
-                <div class="">
-                    <table class="table table-bordered js-basic-example dataTable">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Title</th>
-                                <th>Division</th>
-                                <th>District</th>
-                                <th>Departs Date</th>
-                                <th>Return Date</th>
-                                <th>Deadline Date</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        @php
-                        $i = 1
-                        @endphp
-                        <tbody>
-                            @if ($packages->count())
-                                @foreach ($packages as $p)
-                                    <tr>
-                                        <td>{{ $i++ }}</td>
-                                        <td>{{ $p->title }}</td>
-                                        <td>{{ $p->division->name }}</td>
-                                        <td>{{ $p->district->name }}</td>
-                                        <td>{{ $p->departs_date }}</td>
-                                        <td>{{ $p->return_date }}</td>
-                                        <td>{{ $p->booking_deadline }}</td>
-                                        <td>{{ $p->status }}</td>
-                                        <td>
-                                            <a class="btn btn-sm btn-info" href="{{route('packages.show', $p->id)}}"> Show</a>
-                                            @if ($p->getOriginal('status'))
-                                                <a class="btn btn-xm btn-danger" href="{!! route('packages.is_active',$p->id) !!}">Deactive</a>
-                                            @else
-                                                <a class="btn btn-xm btn-success" href="{!! route('packages.is_active',$p->id) !!}">Active</a>
-                                            @endif
-                                            <a class="btn btn-sm btn-danger" type="button" data-toggle="modal" data-target="#{{ $p->id }}">Delete</a>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="header">
+                    <h2>Package</h2>
+                    <ul class="header-dropdown m-r--5">
+                        <li class="dropdown">
+                            <a class="btn btn-primary" href="{!! route('packages.create') !!}">+ Add New</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="body">
+                    <div class="">
+                        <table class="table table-bordered js-basic-example dataTable">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Title</th>
+                                    <th>Division</th>
+                                    <th>District</th>
+                                    <th>Departs Date</th>
+                                    <th>Return Date</th>
+                                    <th>Deadline Date</th>
+                                    <th>Total Seat</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            @php
+                            $i = 1
+                            @endphp
+                            <tbody>
+                                @if ($packages->count())
+                                    @foreach ($packages as $p)
+                                        <tr>
+                                            <td>{{ $i++ }}</td>
+                                            <td>{{ $p->title }}</td>
+                                            <td>{{ $p->division->name }}</td>
+                                            <td>{{ $p->district->name }}</td>
+                                            <td>{{ $p->departs_date }}</td>
+                                            <td>{{ $p->return_date }}</td>
+                                            <td>{{ $p->booking_deadline }}</td>
+                                            <td>{{ $p->total_seat }}</td>
+                                            <td>{{ $p->status }}</td>
+                                            <td>
+                                                <a class="btn btn-sm btn-info" href="{{route('packages.show', $p->id)}}"> Show</a>
+                                                @if ($p->getOriginal('status'))
+                                                    <a class="btn btn-xm btn-danger" href="{!! route('packages.is_active',$p->id) !!}">Deactive</a>
+                                                @else
+                                                    <a class="btn btn-xm btn-success" href="{!! route('packages.is_active',$p->id) !!}">Active</a>
+                                                @endif
+                                                <a class="btn btn-sm btn-danger" type="button" data-toggle="modal" data-target="#{{ $p->id }}">Delete</a>
 
-                                            <form action="{{ route('packages.destroy', $p->id) }}" method="post">
-                                                {{ csrf_field() }} {{ method_field('delete') }}
-
-
-
-                                                {{-- some change need
-                                                data-target 1 changed by databse id
-                                                and then id of the next line changed by the same database id
-                                                and must be set form action
-                                                --}}
+                                                <form action="{{ route('packages.destroy', $p->id) }}" method="post">
+                                                    {{ csrf_field() }} {{ method_field('delete') }}
 
 
-                                                {{-- -------------------- delete Pop Up ---------------------------  --}}
-                                                <div class="modal fade" id="{{ $p->id }}" role="dialog">
-                                                    @include('includes.delete')
-                                                </div>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            @endif
-                        </tbody>
-                    </table>
+
+                                                    {{-- some change need
+                                                    data-target 1 changed by databse id
+                                                    and then id of the next line changed by the same database id
+                                                    and must be set form action
+                                                    --}}
+
+
+                                                    {{-- -------------------- delete Pop Up ---------------------------  --}}
+                                                    <div class="modal fade" id="{{ $p->id }}" role="dialog">
+                                                        @include('includes.delete')
+                                                    </div>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/admin/package/show.blade.php
+++ resources/views/admin/package/show.blade.php
@@ -2,118 +2,125 @@
 
 @section('content')
     <div class="row clearfix">
-        <div class="card">
-            <div class="header">
-                <h2>Package</h2>
-            </div>
-            <div class="body">
-                <table class="table no-border">
-                    <tr>
-                        <td width="120px">Package Title</td>
-                        <td width="1px">:</td>
-                        <td>{{ $package->title }}</td>
-                    </tr>
-                    <tr>
-                        <td width="120px">Departs Date</td>
-                        <td width="1px">:</td>
-                        <td>{{ $package->departs_date }}</td>
-                    </tr>
-                    <tr>
-                        <td width="120px">Return Date</td>
-                        <td width="1px">:</td>
-                        <td>{{ $package->return_date }}</td>
-                    </tr>
-                    <tr>
-                        <td width="120px">Duration</td>
-                        <td width="1px">:</td>
-                        <td>{{ $package->getInterval()>1? $package->getInterval().' days' : '1 day' }}</td>
-                    </tr>
-                    <tr>
-                        <td width="120px">Deadline Date</td>
-                        <td width="1px">:</td>
-                        <td>{{ $package->booking_deadline }}</td>
-                    </tr>
-                    <tr>
-                        <td width="120px">Status</td>
-                        <td width="1px">:</td>
-                        <td>{{ $package->status }}</td>
-                    </tr>
-                    <tr>
-                        <td width="120px">Per Head Cost</td>
-                        <td width="1px">:</td>
-                        <td>{{ $package->cost }}</td>
-                    </tr>
-                    <tr>
-                        <td width="120px">Type</td>
-                        <td width="1px">:</td>
-                        <td>
-                            @if ($num_of_types = $package->types->count())
-                                @foreach ($package->types as $type)
-                                    {{ $type->type }} {{ 1<$num_of_types--?',':'' }}
-                                @endforeach
-                            @endif
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="3"><a class="btn btn-sm btn-primary pull-right" href="{!! route('packages.edit',$package->id) !!}">Update</a></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-        <div class="card">
-            <div class="header">
-                <h2>Place & Hotel</h2>
-            </div>
-            <div class="body">
-                <table class="table no-border">
-                    <thead>
-                        <th width="30px">Place</th>
-                        <th></th>
-                    </thead>
-                    <tbody>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="header">
+                    <h2>Package</h2>
+                </div>
+                <div class="body">
+                    <table class="table no-border">
                         <tr>
-                            <td></td>
+                            <td width="120px">Package Title</td>
+                            <td width="1px">:</td>
+                            <td>{{ $package->title }}</td>
+                        </tr>
+                        <tr>
+                            <td width="120px">Departs Date</td>
+                            <td width="1px">:</td>
+                            <td>{{ $package->departs_date }}</td>
+                        </tr>
+                        <tr>
+                            <td width="120px">Return Date</td>
+                            <td width="1px">:</td>
+                            <td>{{ $package->return_date }}</td>
+                        </tr>
+                        <tr>
+                            <td width="120px">Duration</td>
+                            <td width="1px">:</td>
+                            <td>{{ $package->getInterval()>1? $package->getInterval().' days' : '1 day' }}</td>
+                        </tr>
+                        <tr>
+                            <td width="120px">Deadline Date</td>
+                            <td width="1px">:</td>
+                            <td>{{ $package->booking_deadline }}</td>
+                        </tr>
+                        <tr>
+                            <td width="120px">Total Seat</td>
+                            <td width="1px">:</td>
+                            <td>{{ $package->total_seat }}</td>
+                        </tr>
+                        <tr>
+                            <td width="120px">Status</td>
+                            <td width="1px">:</td>
+                            <td>{{ $package->status }}</td>
+                        </tr>
+                        <tr>
+                            <td width="120px">Per Head Cost</td>
+                            <td width="1px">:</td>
+                            <td>{{ $package->cost }}</td>
+                        </tr>
+                        <tr>
+                            <td width="120px">Type</td>
+                            <td width="1px">:</td>
                             <td>
-                                @foreach ($package->places as $place)
-                                    <div class="col-md-3">
-                                        <div class="img-thumbnail">
-                                            <img width="100%" class="" src="{{ $place->placeImages[0]->image }}" alt="">
-                                            <label style="font-size:15px;padding-top:10px" for=" value.slug "> {{ $place->title }} </label>
-                                        </div>
-                                    </div>
-                                @endforeach
+                                @if ($num_of_types = $package->types->count())
+                                    @foreach ($package->types as $type)
+                                        {{ $type->type }} {{ 1<$num_of_types--?',':'' }}
+                                    @endforeach
+                                @endif
                             </td>
                         </tr>
-                    </tbody>
-                </table>
-                <table class="table no-border">
-                    <thead>
-                        <th width="30px">Hotel</th>
-                        <th></th>
-                    </thead>
-                    <tbody>
                         <tr>
-                            <td></td>
-                            <td>
-                                @foreach ($package->hotels as $hotel)
-                                    <div class="col-md-3">
-                                        <div class="img-thumbnail">
-                                            <img width="100%" class="" src="{{ $hotel->hotelImages[0]->image }}" alt="">
-                                            <label style="font-size:15px;padding-top:10px" for=" value.slug "> {{ $hotel->name }} </label>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </td>
+                            <td colspan="3"><a class="btn btn-sm btn-primary pull-right" href="{!! route('packages.edit',$package->id) !!}">Update</a></td>
                         </tr>
-                    </tbody>
-                </table>
-                <table class="table no-border">
-                    <tr>
-                        <td colspan="3"><a class="btn btn-sm btn-primary pull-right" href="{!! route('packages.edit-place-hotel',$package->id) !!}">Update</a></td>
-                    </tr>
-                </table>
+                    </table>
+                </div>
+            </div>
+            <div class="card">
+                <div class="header">
+                    <h2>Place & Hotel</h2>
+                </div>
+                <div class="body">
+                    <table class="table no-border">
+                        <thead>
+                            <th width="30px">Place</th>
+                            <th></th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    @foreach ($package->places as $place)
+                                        <div class="col-md-3">
+                                            <div class="img-thumbnail">
+                                                <img width="100%" class="" src="{{ $place->placeImages[0]->image }}" alt="">
+                                                <label style="font-size:15px;padding-top:10px" for=" value.slug "> {{ $place->title }} </label>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="table no-border">
+                        <thead>
+                            <th width="30px">Hotel</th>
+                            <th></th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    @foreach ($package->hotels as $hotel)
+                                        <div class="col-md-3">
+                                            <div class="img-thumbnail">
+                                                <img width="100%" class="" src="{{ $hotel->hotelImages[0]->image }}" alt="">
+                                                <label style="font-size:15px;padding-top:10px" for=" value.slug "> {{ $hotel->name }} </label>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table class="table no-border">
+                        <tr>
+                            <td colspan="3"><a class="btn btn-sm btn-primary pull-right" href="{!! route('packages.edit-place-hotel',$package->id) !!}">Update</a></td>
+                        </tr>
+                    </table>
 
 
+                </div>
             </div>
         </div>
     </div>
